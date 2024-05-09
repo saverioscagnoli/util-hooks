1f08c672df50df786248c64afb35a47581be9640
svscagn <95577971+saverioscagnoli@users.noreply.github.com>
2024-05-09 21:12:15 +0100

    added 2 version of use-hotkey by mistake :(

--- packages/use-hotkey/src/index.ts
+++ packages/use-hotkey/src/index.ts
@@ -21,26 +21,42 @@ function useHotkey<U extends Element, T extends Target<U>>(
   deps?: DependencyList
 ): void;
 
-function useHotkey<U extends Element, T extends Target<U>>(
-  modsOrTarget: T | Modifier[],
-  keyOrMods: Key | Modifier[],
-  cbOrKey: ((evt: KeyboardEvent) => void) | Key,
-  depsOrCb?: DependencyList | ((evt: KeyboardEvent) => void)
-) {
-  const [mods, key, cb, deps] = Array.isArray(modsOrTarget)
-    ? [
-        modsOrTarget,
-        keyOrMods as Key,
-        cbOrKey as (evt: KeyboardEvent) => void,
-        depsOrCb as DependencyList
-      ]
-    : [
-        keyOrMods as Modifier[],
-        cbOrKey as (evt: KeyboardEvent) => void,
-        depsOrCb as DependencyList
-      ];
+function useHotkey(
+  key: Key,
+  cb: (evt: KeyboardEvent) => void,
+  deps?: DependencyList
+): void;
 
-  const target = Array.isArray(modsOrTarget) ? document : modsOrTarget;
+function useHotkey<U extends Element, T extends Target<U>>(
+  modsOrTargetOrKey: T | Modifier[] | Key,
+  keyOrModsOrCb: Key | Modifier[] | ((evt: KeyboardEvent) => void),
+  cbOrKeyOrDeps?: ((evt: KeyboardEvent) => void) | Key | DependencyList,
+  depsOrCbOrNothing?: DependencyList | ((evt: KeyboardEvent) => void)
+) {
+  let mods: Modifier[],
+    key: Key,
+    cb: (evt: KeyboardEvent) => void,
+    deps: DependencyList | undefined;
+  let target: T | Document;
+
+  if (typeof modsOrTargetOrKey === "string") {
+    mods = [];
+    key = modsOrTargetOrKey;
+    cb = keyOrModsOrCb as (evt: KeyboardEvent) => void;
+    deps = cbOrKeyOrDeps as DependencyList;
+    target = document;
+  } else if (Array.isArray(modsOrTargetOrKey)) {
+    mods = modsOrTargetOrKey;
+    key = keyOrModsOrCb as Key;
+    cb = cbOrKeyOrDeps as (evt: KeyboardEvent) => void;
+    deps = depsOrCbOrNothing as DependencyList;
+    target = document;
+  } else {
+    target = modsOrTargetOrKey;
+    mods = keyOrModsOrCb as Modifier[];
+    key = cbOrKeyOrDeps as Key;
+    cb = depsOrCbOrNothing as (evt: KeyboardEvent) => void;
+  }
 
   useEvent(
     target,
@@ -48,6 +64,7 @@ function useHotkey<U extends Element, T extends Target<U>>(
     e => {
       if (mods.every(mod => e[mod]) && deburr(e.key.toLowerCase()) === key) {
         cb(e);
+        return true;
       }
     },
     deps
